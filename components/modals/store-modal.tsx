1a332f60000b9f60bd04e5af889850905d53e943
Muchammad Fahmi Arif <fahmiarif96@gmail.com>
2023-09-01 10:49:20 +0700

    add form in modal components and show on console

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,14 +1,64 @@
 "use client";
 
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 import useStoreModal from "@/hooks/use-store-modal";
+
 import Modal from "@/components/ui/modal";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+const formSchema = z.object({
+  name: z.string().min(3, "Name must be at least 3 characters"),
+});
 
 const StoreModal = () => {
   const storeModal = useStoreModal();
 
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
   return (
     <Modal title="Title Modal" description="Description Modal" isOpen={storeModal.isOpen} onClose={storeModal.onClose}>
-      Children Modal
+      <div className="py-2 space-y-4 pb-4">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder="Name" />
+                  </FormControl>
+                  <FormDescription></FormDescription>
+                  <FormMessage>{form.formState.errors.name?.message}</FormMessage>
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
+        <div className="pt-6 space-x-2 flex items-center justify-end">
+          <Button variant="outline" onClick={storeModal.onClose}>
+            Cancel
+          </Button>
+          <Button type="submit" onClick={form.handleSubmit(onSubmit)}>
+            Submit
+          </Button>
+        </div>
+      </div>
     </Modal>
   );
 };
