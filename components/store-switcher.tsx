c78a85f2478f9b1e1fd389ca76f703c0f34a9375
muchfahmiarif <fahmiarif96@gmail.com>
2023-09-07 22:32:07 +0700

    💄 add data items on props

--- components/store-switcher.tsx
+++ components/store-switcher.tsx
@@ -1,11 +1,16 @@
+"use client";
+
 import React from "react";
 import { PopoverTrigger } from "@/components/ui/popover";
+import { Store } from "@prisma/client";
 
 type PopoverTriggerProps = React.ComponentPropsWithoutRef<typeof PopoverTrigger>;
 
-interface StoreSwitcherProps extends PopoverTriggerProps {}
+interface StoreSwitcherProps extends PopoverTriggerProps {
+  items: Store[];
+}
 
-const StoreSwitcher = () => {
+const StoreSwitcher = ({ className, items = [] }: StoreSwitcherProps) => {
   return <div>StoreSwitcher</div>;
 };
 
