c67c17f516cab304d16a48765e4eb234fd105a16
muchfahmiarif <fahmiarif96@gmail.com>
2023-09-07 22:28:30 +0700

    💄 add switcher store and type data

---
+++ components/store-switcher.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { PopoverTrigger } from "@/components/ui/popover";
+
+type PopoverTriggerProps = React.ComponentPropsWithoutRef<typeof PopoverTrigger>;
+
+interface StoreSwitcherProps extends PopoverTriggerProps {}
+
+const StoreSwitcher = () => {
+  return <div>StoreSwitcher</div>;
+};
+
+export default StoreSwitcher;
