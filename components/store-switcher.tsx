c273c11e2bac86227224814d8f70f4149f61a880
muchfahmiarif <fahmiarif96@gmail.com>
2023-09-07 23:06:57 +0700

    compare id on database store with URL

--- components/store-switcher.tsx
+++ components/store-switcher.tsx
@@ -3,6 +3,8 @@
 import React from "react";
 import { PopoverTrigger } from "@/components/ui/popover";
 import { Store } from "@prisma/client";
+import useStoreModal from "@/hooks/use-store-modal";
+import { useParams, useRouter } from "next/navigation";
 
 type PopoverTriggerProps = React.ComponentPropsWithoutRef<typeof PopoverTrigger>;
 
@@ -11,6 +13,17 @@ interface StoreSwitcherProps extends PopoverTriggerProps {
 }
 
 const StoreSwitcher = ({ className, items = [] }: StoreSwitcherProps) => {
+  const storeModal = useStoreModal();
+  const params = useParams();
+  const router = useRouter();
+
+  const formatedItems = items.map((item) => ({
+    label: item.name,
+    value: item.id,
+  }));
+
+  const curretStore = formatedItems.find((item) => item.value === params.storeId);
+
   return <div>StoreSwitcher</div>;
 };
 
