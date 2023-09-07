8d4287ec0ae06833edd47c40048cd915ede62022
muchfahmiarif <fahmiarif96@gmail.com>
2023-09-07 22:03:48 +0700

    💄 add main nav

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,12 +1,13 @@
 import { UserButton } from "@clerk/nextjs";
 import React from "react";
+import MainNav from "./main-nav";
 
 const Navbar = () => {
   return (
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
         <div>This will be a store switcher</div>
-        <div>This will be the routes</div>
+        <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
           <UserButton afterSignOutUrl="/" />
         </div>
