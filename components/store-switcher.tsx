13653d0dde5c04024600ee9253798a45aeb699bc
muchfahmiarif <fahmiarif96@gmail.com>
2023-09-07 23:20:23 +0700

    💄 add function store select and popover

--- components/store-switcher.tsx
+++ components/store-switcher.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import React from "react";
-import { PopoverTrigger } from "@/components/ui/popover";
+import { Popover, PopoverTrigger } from "@/components/ui/popover";
 import { Store } from "@prisma/client";
 import useStoreModal from "@/hooks/use-store-modal";
 import { useParams, useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
 
 type PopoverTriggerProps = React.ComponentPropsWithoutRef<typeof PopoverTrigger>;
 
@@ -16,15 +17,28 @@ const StoreSwitcher = ({ className, items = [] }: StoreSwitcherProps) => {
   const storeModal = useStoreModal();
   const params = useParams();
   const router = useRouter();
+  const [open, setOpen] = React.useState(false);
 
   const formatedItems = items.map((item) => ({
     label: item.name,
     value: item.id,
   }));
 
-  const curretStore = formatedItems.find((item) => item.value === params.storeId);
+  const currentStore = formatedItems.find((item) => item.value === params.storeId);
 
-  return <div>StoreSwitcher</div>;
+  const onStoreSelected = (store: { label: string; value: string }) => {
+    // if clicked popover will be close and move to store page
+    setOpen(false);
+    router.push(`/${store.value}`);
+  };
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger>
+        <Button></Button>
+      </PopoverTrigger>
+    </Popover>
+  );
 };
 
 export default StoreSwitcher;
