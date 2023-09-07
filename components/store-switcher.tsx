ceb11cc870db92e5658c3d00d9a1d73d5c1666ca
muchfahmiarif <fahmiarif96@gmail.com>
2023-09-08 06:08:52 +0700

    💄 add ui store switcher

--- components/store-switcher.tsx
+++ components/store-switcher.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import React from "react";
-import { Popover, PopoverTrigger } from "@/components/ui/popover";
+import { Check, ChevronsUpDown, Store as StoreIcon } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { Store } from "@prisma/client";
 import useStoreModal from "@/hooks/use-store-modal";
-import { useParams, useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList, CommandSeparator } from "@/components/ui/command";
 
 type PopoverTriggerProps = React.ComponentPropsWithoutRef<typeof PopoverTrigger>;
 
@@ -35,7 +39,36 @@ const StoreSwitcher = ({ className, items = [] }: StoreSwitcherProps) => {
   return (
     <Popover open={open} onOpenChange={setOpen}>
       <PopoverTrigger>
-        <Button></Button>
+        <Button
+          variant={`outline`}
+          size={`sm`}
+          aria-expanded={open}
+          aria-label="Selec a store"
+          className={cn("w-[200px] justify-between", className)}>
+          <StoreIcon className="mr-2 h-4 w-4" />
+          Current Store
+          <ChevronsUpDown className="ml-auto h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+        <PopoverContent className="w-[200px] p-0">
+          <Command>
+            <CommandInput placeholder="Search store..." />
+            <CommandEmpty>No store found.</CommandEmpty>
+            <CommandList>
+              <CommandGroup>
+                {formatedItems.map((store) => (
+                  <CommandItem key={store.value} onSelect={() => onStoreSelected(store)} className="text-sm">
+                    <Check className={cn("mr-2 h-4 w-4", currentStore?.value === store.value ? "opacity-100" : "opacity-0")} />
+                    {store.label}
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </CommandList>
+            <CommandSeparator />
+            <CommandList>
+              <CommandGroup></CommandGroup>
+            </CommandList>
+          </Command>
+        </PopoverContent>
       </PopoverTrigger>
     </Popover>
   );
