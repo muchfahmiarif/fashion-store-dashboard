0e725a3a1a3d09fbf948288f4db13fb51afa7e2e
Muchammad Fahmi Arif <fahmiarif96@gmail.com>
2023-09-08 13:51:51 +0700

    🚩 add new api for update name store

---
+++ app/api/stores/[storeId]/route.ts
@@ -0,0 +1,39 @@
+import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs";
+import prismadb from "@/lib/prismadb";
+
+export async function PATCH(req: Request, res: Response, { params }: { params: { storeId: string } }) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+
+    const { name } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!name) {
+      return new NextResponse("Missing name", { status: 400 });
+    }
+
+    if (!params.storeId) {
+      return new NextResponse("Missing storeId", { status: 400 });
+    }
+
+    const store = await prismadb.store.updateMany({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+      data: {
+        name,
+      },
+    });
+
+    return NextResponse.json(store);
+  } catch (error) {
+    console.log("[STORES_PATCH]", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
