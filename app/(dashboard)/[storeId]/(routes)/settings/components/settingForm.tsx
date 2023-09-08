7a1649c3021c45e11e531628c4d441f0ca9711aa
Muchammad Fahmi Arif <fahmiarif96@gmail.com>
2023-09-08 11:36:21 +0700

    💄 add input and button show on console

--- app/(dashboard)/[storeId]/(routes)/settings/components/settingForm.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settingForm.tsx
@@ -10,6 +10,8 @@ import { Button } from "@/components/ui/button";
 import Heading from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 import { Store } from "@prisma/client";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 interface SettingsFormProps {
   initialData: Store;
@@ -48,6 +50,29 @@ const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
         </Button>
       </div>
       <Separator />
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name={`name`}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Store Name" {...field} disabled={loading} />
+                  </FormControl>
+                  <FormDescription>Update your store name.</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button type="submit" disabled={loading}>
+            Save change
+          </Button>
+        </form>
+      </Form>
     </>
   );
 };
