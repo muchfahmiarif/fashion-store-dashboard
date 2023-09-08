623664335ff2802f5d9a111c4240fe4a71edb401
Muchammad Fahmi Arif <fahmiarif96@gmail.com>
2023-09-08 11:22:48 +0700

    💬 add validation form on settings

--- app/(dashboard)/[storeId]/(routes)/settings/components/settingForm.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settingForm.tsx
@@ -1,16 +1,39 @@
 "use client";
 
+import React from "react";
+import { Trash2Icon } from "lucide-react";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
 import { Button } from "@/components/ui/button";
 import Heading from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
 import { Store } from "@prisma/client";
-import { Trash2Icon } from "lucide-react";
-import React from "react";
 
 interface SettingsFormProps {
   initialData: Store;
 }
 
+const formSchema = z.object({
+  name: z.string().min(3).max(255),
+});
+
+type SettingFormValue = z.infer<typeof formSchema>;
+
 const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
+  const [open, setOpen] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+
+  const form = useForm<SettingFormValue>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData,
+  });
+
+  const onSubmit = async (values: SettingFormValue) => {
+    console.log(values);
+  };
+
   return (
     <>
       <div className="flex items-center justify-between">
@@ -24,6 +47,7 @@ const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
           <Trash2Icon className="h-4 w-4" />
         </Button>
       </div>
+      <Separator />
     </>
   );
 };
