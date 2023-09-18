5b8ed78e4071c5f14c7f84d6cd455c78733df9e7
Muchammad Fahmi Arif <fahmiarif96@gmail.com>
2023-09-18 21:38:40 +0700

    `billboard` add api for get data billboard

--- app/api/[storeId]/billboards/route.ts
+++ app/api/[storeId]/billboards/route.ts
@@ -9,7 +9,7 @@ export async function POST(req: Request, { params }: { params: { storeId: string
     const { label, imageUrl } = body;
 
     if (!userId) {
-      return new NextResponse("Unauthorized", { status: 401 });
+      return new NextResponse("Unauthenticated", { status: 401 });
     }
 
     if (!label) {
@@ -24,6 +24,17 @@ export async function POST(req: Request, { params }: { params: { storeId: string
       return new NextResponse("Store ID is required", { status: 400 });
     }
 
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorized", { status: 403 });
+    }
+
     const billboard = await prismadb.billboard.create({
       data: {
         label,
@@ -35,6 +46,25 @@ export async function POST(req: Request, { params }: { params: { storeId: string
     return NextResponse.json(billboard);
   } catch (error) {
     console.error("[BILLBOARDS_POST]", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
+
+export async function GET(req: Request, { params }: { params: { storeId: string } }) {
+  try {
+    if (!params.storeId) {
+      return new NextResponse("Store ID is required", { status: 400 });
+    }
+
+    const billboard = await prismadb.billboard.findMany({
+      where: {
+        storeId: params.storeId,
+      },
+    });
+
+    return NextResponse.json(billboard);
+  } catch (error) {
+    console.error("[BILLBOARDS_GET]", error);
     return new NextResponse("Internal Server Error", { status: 500 });
   }
 }
